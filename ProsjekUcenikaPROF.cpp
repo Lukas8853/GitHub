a303185025778b95afec2025fb836643ddb84379
Lukas8853 <lukas.1107pvt@gmail.com>
2024-05-23 19:17:57 +0200

    DZ upgrade
    izi work

---
+++ ProsjekUcenikaPROF.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+struct Ucenik
+{
+    char imePrezime[50];
+    float prosjek;
+};
+bool cmp(Ucenik &a, Ucenik &b)
+{
+    return a.prosjek > b.prosjek;
+}
+int main()
+{
+    struct Ucenik ucenici[100];
+    int brUcenika = 0;
+    fstream datoteka("data.bin", ios::binary | ios::in);
+    while (datoteka.read((char *)&ucenici[brUcenika], sizeof(Ucenik)))
+    {
+        cout << ucenici[brUcenika].imePrezime << " " << ucenici[brUcenika].prosjek << endl;
+        brUcenika++;
+    }
+    datoteka.close();
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin.getline(ucenici[brUcenika + i].imePrezime, 50);
+        cin >> ucenici[brUcenika + i].prosjek;
+    }
+    sort(ucenici, ucenici + brUcenika + n, cmp);
+    datoteka.open("data.bin", ios::binary | ios::out | ios::trunc);
+    datoteka.write((char *)ucenici, sizeof(Ucenik) * (brUcenika + n));
+    datoteka.close();
+    return 0;
+}
