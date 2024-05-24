3de485da46cf215ed5c4f261fbcf59a64ac3d484
Lukas8853 <lukas.1107pvt@gmail.com>
2024-05-24 09:10:18 +0200

    dz upgrade
    upgrade sa cin.ignore()

--- ProsjekUcenikaPROF.cpp
+++ ProsjekUcenikaPROF.cpp
@@ -12,11 +12,11 @@ bool cmp(Ucenik &a, Ucenik &b)
 {
     return a.prosjek > b.prosjek;
 }
-int main()
+int main() 
 {
     struct Ucenik ucenici[100];
     int brUcenika = 0;
-    fstream datoteka("data.bin", ios::binary | ios::in);
+    fstream datoteka("C:/Users/Gb-gama/Documents/GitHub/GitHub/data.bin", ios::binary | ios::in);
     while (datoteka.read((char *)&ucenici[brUcenika], sizeof(Ucenik)))
     {
         cout << ucenici[brUcenika].imePrezime << " " << ucenici[brUcenika].prosjek << endl;
@@ -25,14 +25,17 @@ int main()
     datoteka.close();
     int n;
     cin >> n;
+    cin.ignore();
     for (int i = 0; i < n; i++)
     {
         cin.getline(ucenici[brUcenika + i].imePrezime, 50);
         cin >> ucenici[brUcenika + i].prosjek;
+        cin.ignore();
     }
     sort(ucenici, ucenici + brUcenika + n, cmp);
-    datoteka.open("data.bin", ios::binary | ios::out | ios::trunc);
+    datoteka.open("C:/Users/Gb-gama/Documents/GitHub/GitHub/data.bin", ios::binary | ios::out | ios::trunc);
     datoteka.write((char *)ucenici, sizeof(Ucenik) * (brUcenika + n));
     datoteka.close();
     return 0;
 }
+// C:/Users/Gb-gama/Documents/GitHub/GitHub/data.bin je path na sosarovom racunalu
